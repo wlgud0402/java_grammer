f532012e5af723bf2c42b3840e9752a9e3188a9a
wlgud0402 <wlgudrlgus@naver.com>
2020-05-30 00:22:31 +0900

    더하기 빼기 곱하기 나누기

---
+++ calcul.java
@@ -0,0 +1,12 @@
+public class calcul {
+    public static void main(String[] args) {
+    //더하기 빼기 곱하기 나누기 ( + , - , * , / )
+    int a = 12;
+    int b = 12;
+
+    int total = b + a;
+    
+    System.out.println("total is " + total);
+    System.out.println(10 + total);
+    }
+}
