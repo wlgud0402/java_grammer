65cf8c3b58cd7458389feca1e2f68f2c267031d7
wlgud0402 <wlgudrlgus@naver.com>
2020-05-30 00:23:14 +0900

    어긋난 타입 캐스팅

--- casting.java
+++ casting.java
@@ -1,15 +1,6 @@
 public class casting {
     public static void main(String[] args) {
 
-        //더하기 빼기 곱하기 나누기 ( + , - , * , / )
-        int a = 12;
-        int b = 12;
-
-        int total = b + a;
-        
-        System.out.println("total is " + total);
-        System.out.println(10 + total);
-
         int c = 12;
         double d = 12;
 
