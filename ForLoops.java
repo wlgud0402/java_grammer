67b25efa2869f213e223af07b9acc8d68ded8c89
wlgud0402 <wlgudrlgus@naver.com>
2020-05-31 23:27:27 +0900

    반복문

---
+++ ForLoops.java
@@ -0,0 +1,13 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            if (i % 2 == 0){
+                System.out.println(i+ " is multiple of 2");
+            }
+        }
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Hello there  " + i);
+        }
+    }
+}
