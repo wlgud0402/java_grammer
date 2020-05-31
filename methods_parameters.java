78905c958a01016a10544f35bd5dc793dda25432
wlgud0402 <wlgudrlgus@naver.com>
2020-06-01 00:05:47 +0900

    인자, if문이 들어간 메소드

--- methods_parameters.java
+++ methods_parameters.java
@@ -1,7 +1,7 @@
 public class methods_parameters {
     public static void main(String[] args) {
         showName();
-        anotherMethods("hello"); //something: "넣고싶은말" 
+        anotherMethods("hello",23); //something: "넣고싶은말" 
         //anotherMethods 의 출력은 another methods + something 여기서는 "hello"
     }
     public static void showName(){
@@ -9,7 +9,11 @@ public class methods_parameters {
     }
 
 
-    public static void anotherMethods(String something){ //안에 넣고싶은 인자의 타입을 정함
-        System.out.println("another methods " + something); 
+    public static void anotherMethods(String something, int age){
+        if(age<90){
+            System.out.println("I am still young!!");
+        }
+
+        System.out.println("another methods " + something +" "+ age);
     }
 }
