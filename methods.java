21c31264fc091e1d087bf382bae49a8e43b7afe6
wlgud0402 <wlgudrlgus@naver.com>
2020-05-31 23:27:56 +0900

    메소드

---
+++ methods.java
@@ -0,0 +1,9 @@
+public class methods {
+    public static void main(String[] args) {
+        showName(); // 아래서 정의한 함수를 여기서 실행
+
+    }
+    public static void showName(){
+        System.out.println("From ShowName");
+    }
+}
