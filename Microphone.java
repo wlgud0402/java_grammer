f3c2cea2a4f2c1d300d21bcb1de0412053e35216
wlgud0402 <wlgudrlgus@naver.com>
2020-06-01 14:08:45 +0900

    클래스, 메소드, 액션, 오버라이딩, 컨스트럭터, 게터&세터

--- Microphone.java
+++ Microphone.java
@@ -1,6 +1,64 @@
 public class Microphone {
     //instance variables / properties / fields
-    String name;
-    String color;
-    int model;
+    private String name;
+    private String color;
+    private int model;
+
+    //비어있는 constructor나 더 적은수의 인자를 가진것을 만들어도 괜찮음 몇개든 제작가능! //but다른 것과 아에 같은것은 안됨.
+    public Microphone(){}
+    
+    public Microphone(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+
+    //Alt + Insert => generate code => constructor// 컨스트럭터 사용으로 다른 곳에서 사용할때 더 간편함
+    //위에서 정의한 클래스명과 같아야함
+    public Microphone(String name, String color, int model) {
+        this.name = name;
+        this.color = color;
+        this.model = model;
+    }
+
+    //Alt + Insert
+    //Getters And Setters =>오버라이드 할수 있게 따로 도와줌
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+
+    //Actions methods => 다른곳에서 사용할수 있도록 정의해줌
+    public void turnOff(){
+        System.out.println(this.name + " Turn Off"); //this.name => 메소드의 name
+    }
+    public void turnOn(){
+        System.out.println(this.name + " Turn On");
+    }
+    public void setVolume(){
+        System.out.println(this.name + " Setting volume");
+    }
+
+    public String showDescription(){
+        return "Microphone name " + "with color: " + this.color
+                + " and its model " + this.model;
+    }
 }
--- Microphone_use.java
+++ Microphone_use.java
@@ -1,16 +1,34 @@
 public class Microphone_use {
     
     public static void main(String[] args){
-        Microphone microphone = new Microphone(); //객체를 인스턴스화 instantiating our object
-        microphone.color = "Blue";
-        microphone.name = "Blue Yeti";
-        microphone.model = 13345;
 
-        Microphone gHMic = new Microphone(); //위의 객체와는 다른 Microphone 의 인스턴스
-        gHMic.model = 456;
-        gHMic.name = "GHZB";
-        gHMic.color = "Red";
+        //constructor를 생성해서 사용하기전에는 함수를 정의하고 원하는 값들을 따로 microphone.블라블라로 정의했지만 
+        //이를 사용함으로써 좀더 간단해진다.
 
+        // Microphone microphone = new Microphone(); //객체를 인스턴스화 instantiating our object
+        // microphone.color = "Blue";
+        // microphone.name = "Blue Yeti";
+        // microphone.model = 13345;
+
+        // Microphone gHMic = new Microphone(); //위의 객체와는 다른 Microphone 의 인스턴스
+        // gHMic.model = 456;
+        // gHMic.name = "GHZB";
+        // gHMic.color = "Red";
+
+
+        //위에서 counstructor없이 사용할때는 값들을 다 정해주느라 코드가 길다.
+        Microphone microphone = new Microphone("Blue Yeti", "Blue", 364663);
+        Microphone gHMic = new Microphone("GHMic","red", 546);
+
+        //오버라이드한값들을 getter와 setter를 사용해서 새로 사용해줌
+        microphone.setModel(87);
+        microphone.setName("New Blue Yeti");
+        microphone.setColor("Red Blue");
+
+        System.out.println("New Mic: " + microphone.getModel());
+
+
+        /*
         System.out.println(gHMic.color); //color, name, model 없이 gHMic 으로 접근하면 객체가 출력됨 like 맵
         System.out.println(gHMic.name);
         System.out.println(gHMic.model);
@@ -18,5 +36,30 @@ public class Microphone_use {
         System.out.println(microphone.color);
         System.out.println(microphone.name);
         System.out.println(microphone.model);
+        */
+
+
+        //Acitons methods 사용
+        microphone.turnOn();
+        microphone.setVolume();
+        microphone.turnOff();
+
+        gHMic.turnOn();
+        gHMic.setVolume();
+        gHMic.turnOff();
+
+        System.out.println(microphone.showDescription());
+
+        //새로 만든 constructor
+        Microphone newMic = new Microphone("NewMic","Green",123);
+        Microphone otherMic = new Microphone();
+        otherMic.setColor("Other Mic");
+
+        System.out.println(otherMic.getColor());
+        System.out.println(newMic);
+
+        //좀 다른 형태로 만들어서 오버라이딩
+        Microphone grandMic = new Microphone("Grand", "Yellow");
+        System.out.println(grandMic);
     }
 }
